a14af7a16d58cb068edd28135479da40742a9ab6
Chris Hendricks <crshenx@gmail.com>
2022-06-06 15:47:40 -0700

    commit tree ca671aba40fbc13d19e010df163453aa6abffeb9

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride) {
   // Write your code here!
+  if (ride < 400) {
+    return "This one is on me!";
+  } else if (ride > 400 && ride < 2000) {
+    return "That will be twenty bucks.";
+  } else if (ride > 2000 && ride < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (ride > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  }
+  return "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  if (tip === "generous") {
+    return "Thank you so much.";
+  } else if (tip === "not as generous") {
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
 }
+
+/*
+ * it('should return "Thank you so much." if the tip is generous', function () {
+      expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
+    });
+
+    it('should return "Thank you." if the tip is not as generous', function () {
+      expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
+    });
+
+    it('should return "Bye." if anything else', function () {
+      expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
+    }); 
+ * 
+ */
